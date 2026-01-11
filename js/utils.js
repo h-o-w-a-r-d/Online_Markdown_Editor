40bd3159304d7b67f64487f414cd3ba53516ecc4
Howard Zhen <214858809+h-o-w-a-r-d@users.noreply.github.com>
2026-01-11 13:29:22 +0800

    Add debounce function to utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,8 @@
-
+// js/utils.js
+export function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
